9dffaac9e5d49e6c55cd948deaa812742f7725c6
zhooke <709688530@qq.com>
2022-03-13 22:34:21 +0800

    [feature]完成左右侧边栏布局

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,14 +3,61 @@
     <el-container>
       <el-container>
         <el-aside class="left-aside">
+<!--          左侧边栏-->
           <el-scrollbar>
-            <p v-for="item in 20" :key="item" class="scrollbar-demo-item">{{ item }}</p>
+            <p>BLOG MENU</p>
+            <el-row>
+              <h4 style="color: #5c5e6e;margin-left: 20px">MENU</h4>
+              <el-col>
+                <el-menu  class="card-left">
+                  <el-menu-item index="1">
+                    <el-icon style="color: #1B86F9"><eleme-filled /></el-icon>
+                    <span>博客园</span>
+                  </el-menu-item>
+                <el-menu-item index="2">
+                  <el-icon style="color: #00d1b2"><HomeFilled /></el-icon>
+                  <span>首页</span>
+                </el-menu-item>
+                <el-menu-item index="3" >
+                  <el-icon style="color: #ff6b81"><edit-pen /></el-icon>
+                  <span>新随笔</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <el-icon style="color: #10ac84"><box /></el-icon>
+                  <span>草稿箱</span>
+                </el-menu-item>
+                  <el-menu-item index="5">
+                    <el-icon style="color: #ffd257"><promotion /></el-icon>
+                    <span>联系</span>
+                  </el-menu-item>
+                  <el-menu-item index="6">
+                    <el-icon style="color: #00d1b2" ><histogram /></el-icon>
+                    <span>订阅</span>
+                  </el-menu-item>
+                  <el-menu-item index="7">
+                    <el-icon style="color: #48c774" ><setting /></el-icon>
+                    <span>管理</span>
+                  </el-menu-item>
+                </el-menu>
+              </el-col>
+            </el-row>
+            <el-row>
+              <h4 style="color: #5c5e6e;margin-left: 20px">LINKS</h4>
+              <el-col>
+                <el-menu  class="card-left">
+                  <el-menu-item index="1">
+                    <span>博客园</span>
+                  </el-menu-item>
+                </el-menu>
+              </el-col>
+            </el-row>
           </el-scrollbar>
         </el-aside>
         <el-container>
           <el-header>Header</el-header>
           <el-main noresize="true" style="background-color: #f5f6f7">
             <el-scrollbar height="100%" style="width: 100%;">
+<!--              个人资料展示-->
               <el-card class="header-card" :body-style="{padding:'0'}">
                 <el-card class="header-card-background" :body-style="{padding:'0'}">
                 </el-card>
@@ -31,6 +78,7 @@
                   </el-row>
                 </div>
               </el-card>
+<!--              正文部分-->
               <el-card v-for="item in 10" :key="item" class="article-card" :body-style="{padding:'0'}">
                 <el-row style="height: 200px;width: 100%;" type="flex">
                   <el-col :span="6" style="height: 200px">
@@ -92,8 +140,80 @@
           <el-footer>Footer</el-footer>
         </el-container>
         <el-aside>
-          <el-scrollbar>
-            <p v-for="item in 20" :key="item" class="scrollbar-demo-item">{{ item }}</p>
+<!--          右侧边栏-->
+          <el-scrollbar class="card-right">
+            <p>BLOG INFO</p>
+            <el-row justify="start">
+              <el-col :span="3"><a href="/"><el-icon><chat-dot-round /></el-icon></a>
+              </el-col >
+              <el-col :span="3">
+                <a href="/"><el-icon><Bell /></el-icon></a>
+              </el-col>
+              <el-col :span="3">
+                <a href="/"><el-icon> <setting /></el-icon></a>
+              </el-col>
+              <el-col :span="5"  style="margin-left: auto">
+                <a href="/"> <el-icon><star /></el-icon></a>
+              </el-col>
+            </el-row>
+
+            <el-tabs v-model="activeName" class="card-right-tabs" @tab-click="handleClick">
+              <el-tab-pane name="first">
+                <template #label>
+                  <el-icon><tickets /></el-icon>
+                </template>
+                <p>热门文章</p>
+                <el-row v-for="item in 5" :key="item" gutter="5" style="margin-bottom: 20px;height: 75px">
+                  <el-col :span="6">
+                    <el-image :src="text_url" fit="cover"></el-image>
+                  </el-col>
+                  <el-col :span="16">
+                    <span style="margin-bottom: 3px">{{item}}. awk - 提取包含某个关键字的段落</span>
+                    <span style="color: #98a6ad"><el-icon size="16px" style="color: #98a6ad"><video-camera-filled /></el-icon>123</span>
+                </el-col>
+                </el-row>
+                <p>博主信息</p>
+                <el-card style="font-size: 14px;color: #6e6e6e;margin-bottom: 10px">
+                  <el-row >
+                    <el-col :span="12">博主昵称</el-col>
+                    <el-col :span="12">zhooke</el-col>
+                    <el-divider ></el-divider>
+                  </el-row>
+                  <el-row>
+                    <el-col :span="12">运行天数</el-col>
+                    <el-col :span="12">2年2个月</el-col>
+                    <el-divider ></el-divider>
+                  </el-row>
+                  <el-row>
+                    <el-col :span="12">文章数量</el-col>
+                    <el-col :span="12">16</el-col>
+                    <el-divider ></el-divider>
+                  </el-row>
+                  <el-row>
+                    <el-col :span="12">评论数量</el-col>
+                    <el-col :span="12">123</el-col>
+                    <el-divider ></el-divider>
+                  </el-row>
+                  <el-row>
+                    <el-col :span="12">博客粉丝</el-col>
+                    <el-col :span="12">123</el-col>
+                    <el-divider ></el-divider>
+                  </el-row>
+                </el-card>
+               </el-tab-pane>
+              <el-tab-pane name="second">
+                <template #label>
+                  <el-icon><comment /></el-icon>
+                </template>
+                <span>2</span>
+              </el-tab-pane>
+              <el-tab-pane name="third">
+                <template #label>
+                  <el-icon><fold /></el-icon>
+                </template>
+                <span>3</span>
+              </el-tab-pane>
+            </el-tabs>
           </el-scrollbar>
         </el-aside>
       </el-container>
@@ -111,11 +231,13 @@ export default {
     return {
       header_image_url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F4fa94a8d213a0991007a99f37035cc06715b980c558e9-En6TDM_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1649476198&t=9cd9bfde665ffde6b4ebd9ee2cd0ed9a',
       header_url: 'https://img2.baidu.com/it/u=2723317969,2981355550&fm=26&fmt=auto',
+      text_url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Finews.gtimg.com%2Fnewsapp_match%2F0%2F11020033428%2F0.jpg&refer=http%3A%2F%2Finews.gtimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1649771328&t=364f380b566aba545f1acfc37d0b1f85',
       currentPage4: ref(4),
       pageSize4: ref(4),
       small: ref(false),
       background: ref(false),
-      disable: ref(false)
+      disable: ref(false),
+      activeName: ref('first')
     }
   },
   methods: {
@@ -124,6 +246,9 @@ export default {
     },
     handleCurrentChange(val) {
       console.log(`current page: ${val}`)
+    },
+    handleClick(tab, event){
+      console.log(tab, event)
     }
   }
 }
@@ -223,7 +348,6 @@ export default {
   height: 200px;
   width: 100%;
   margin-bottom: 30px;
-  //text-align: center;
   border-radius: 10px;
   background: var(--el-color-primary-light-9);
   color: var(--el-color-primary);
@@ -231,8 +355,7 @@ export default {
 
 .el-aside {
   height: 100vh;
-  width: 200px;
-
+  width: 250px;
   .left-aside {
     background-color: #fff
   }
@@ -241,13 +364,11 @@ export default {
 .el-header {
   height: 5vh;
   width: 100%;
-  //border: #000 1px solid
 }
 
 .el-footer {
   height: 5vh;
   width: 100%;
-  //border: #000 1px solid
 }
 
 .demo-pagination-block + .demo-pagination-block {
@@ -256,7 +377,6 @@ export default {
 
 .demo-pagination-block .demonstration {
   margin-top: 10px;
-
   margin-bottom: 16px;
 }
 
@@ -277,7 +397,7 @@ export default {
   .card-context{
     height: 70%;
     flex-wrap:wrap;
-    height: 100px;
+    min-height: 100px;
     line-height: 25px;
     font-size: 14px;
     color: #a2b0b7;
@@ -304,6 +424,59 @@ export default {
     .el-icon{
       margin-right: 5px;
       font-size: 14px;
+    }
+  }
+}
+
+.card-left{
+  vertical-align: baseline;
+  line-height: 1.5;
+  flex-direction: column;
+  padding-left: 10px;
+  .el-row{
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0;
+    margin-inline-end: 0;
+    padding-inline-start: 40px;
+  }
+  .el-col{
+    display: inline-flex;
+    font-size: 16px;
+  }
+  .el-icon{
+    margin-right: 10px;
+    font-size: 16px;
+  }
+  .el-menu-item{
+    color: #6e6e6e;
+  }
+}
+
+.card-right{
+  vertical-align: baseline;
+  line-height: 1.5;
+  flex-direction: column;
+  padding-left: 10px;
+  margin-right: 10px;
+
+  .el-col{
+    display: inline-flex;
+    flex-direction:column
+  }
+  .el-icon{
+    margin-right: 10px;
+    font-size: 20px;
+    color: #6e6e6e;
+  }
+  .card-right-tabs{
+    text-align: start;
+
+    .el-divider{
+      margin: 10px 0 15px 0;
+    }
+    .el-col{
+      color: #6e6e6e;
     }
   }
 }
