4db1ca8e8b46b40049230b2b83ca33d6ac6434fa
zhooke <709688530@qq.com>
2022-05-10 14:39:12 +0800

    [fix]提取出comment组件

--- src/components/markdown/Read.vue
+++ src/components/markdown/Read.vue
@@ -65,63 +65,18 @@
                     style="width: 100%;z-index: 100">
       </mavon-editor>
       <!--    评论输入框-->
-      <el-card class="comment-card" justify="space-between">
-        <p>发表评论</p>
-        <el-input
-          v-model="commentContext.comment"
-          type="textarea"
-          maxlength="1000"
-          show-word-limit
-          :rows="commentInputRow"
-          placeholder="请发表有价值的评论， 博客评论不欢迎灌水，良好的社区氛围需大家一起维护。"
-          style="border-style: none;"
-        >
-        </el-input>
-        <el-row style="margin-top: 10px;" justify="space-between">
-          <!--        todo 添加表情包和图片功能-->
-          <el-col span="12">
-          </el-col>
-          <el-col span="12">
-            <el-button type="success" icon="Check" @click="commentBlog">发表</el-button>
-          </el-col>
-        </el-row>
-
-        <el-divider content-position="left">评论列表</el-divider>
-        <el-row justify="space-between" v-for="item in commentList" :key="item" style="margin-bottom: 10px">
-          <el-col span="16" class="comment-text-left-col">
-            <el-avatar :size="30" :src="item.headImgUrl === null ?circleUrl :item.headImgUrl" fit="cover"/>
-            <div style="flex-direction:column;margin-left: 10px">
-              <p>{{ item.createUserName === null ? item.ipAddress : item.createUserName }} | {{ item.browserModel }} |
-                {{ item.createDate }}</p>
-              <span class="comment-text">{{ item.comment }}</span>
-            </div>
-          </el-col>
-          <el-col span="8" style="flex-direction:row;margin-top: 10px">
-            <el-button icon="Edit" size="small">回复</el-button>
-            <el-button icon="Check" size="small"/>
-          </el-col>
-        </el-row>
-        <el-pagination
-          v-model:currentPage="commentRequest.pageIndex"
-          :page-size="commentRequest.pageSize"
-          small="small"
-          background
-          layout="total, prev, pager, next"
-          :total="commentRequest.total"
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          v-show="commentRequest.total >10"
-        />
-      </el-card>
+      <comment :blog="blog"></comment>
     </div>
   </el-scrollbar>
 </template>
 
 <script>
-import { commentBlogApi, commentListApi, deleteBlogApi, getBlogByIdApi, getBlogTagApi } from '@/utils/api';
+import { deleteBlogApi, getBlogByIdApi, getBlogTagApi } from '@/utils/api';
+import Comment from '@/components/other/Comment';
 
 export default {
   name: 'ReadContext',
+  components: { Comment },
   data() {
     return {
       markdownOption: {
@@ -132,22 +87,8 @@ export default {
         content: '',
         createUserId: ''
       },
-      commentInputRow: 3,
-      commentText: '',
       circleUrl:
         'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
-      commentButtonShow: false,
-      commentList: [],
-      commentRequest: {
-        pageIndex: 1,
-        pageSize: 10,
-        total: 0
-      },
-      commentContext: {
-        blogId: '',
-        comment: '',
-        browserModel: ''
-      },
       blogTagList: [],
       userinfo: {}
     };
@@ -165,30 +106,6 @@ export default {
     async getBlog(id) {
       const { data: result } = await getBlogByIdApi(id)
       this.blog = result.data
-    },
-    async getCommentList() {
-      this.commentRequest.blogId = this.blog.id
-      const { data: result } = await commentListApi(this.commentRequest)
-      this.commentList = result.data
-      this.commentRequest.pageSize = result.pageSize
-      this.commentRequest.pageIndex = result.pageIndex
-      this.commentRequest.total = result.total
-    },
-    async commentBlog() {
-      this.commentContext.blogId = this.blog.id
-      this.commentContext.blogAuthorId = this.blog.createUserId
-      this.commentContext.browserModel = this.getExplorer()
-      await commentBlogApi(this.commentContext)
-      await this.getCommentList()
-      this.commentContext.comment = ''
-    },
-    handleSizeChange(val) {
-      this.commentRequest.pageSize = val
-      this.getCommentList()
-    },
-    handleCurrentChange(val) {
-      this.commentRequest.pageIndex = val
-      this.getCommentList()
     },
     async getBlogTag() {
       const { data: result } = await getBlogTagApi(this.blog.id)
@@ -208,39 +125,12 @@ export default {
     },
     editBlog() {
       this.$router.push({ name: 'edit', params: this.blog })
-    },
-    getExplorer() {
-
-      const explorer = window.navigator.userAgent;
-      //Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36
-      let start = 0;
-      const compare = function (s) {
-        start = explorer.indexOf(s)
-        return (start >= 0);
-      };
-      const ie11 = (function () {
-        return ('ActiveXObject' in window)
-      })();
-      if (compare('MSIE') || ie11) {
-
-      } else if (compare('Firefox') && !ie11) {
-
-      } else if (compare('Chrome') && !ie11) {
-
-      } else if (compare('Opera') && !ie11) {
-
-      } else if (compare('Safari') && !ie11) {
-
-      }
-      return explorer.substring(start);
     }
   },
   mounted() {
     this.userinfo = JSON.parse(window.sessionStorage.getItem('userinfo'))
-
     this.blog.id = this.$route.query.blogId
     this.getBlog(this.blog.id)
-    this.getCommentList()
     this.getBlogTag()
   }
 }
@@ -273,36 +163,5 @@ export default {
   }
 }
 
-.comment-card {
-  display: inline-block;
-  text-align: left;
-  width: 100%;
-  margin-top: 10px;
 
-  .comment-text {
-    line-height: 25px;
-    //font-size: 14px;
-    //color: #a2b0b7;
-    word-break: break-word;
-    // 文字自动换行
-    white-space: normal;
-  }
-
-  .el-avatar {
-    margin-top: 10px;
-    min-width: 30px;
-    min-height: 30px
-  }
-
-  .comment-text-left-col {
-    display: flex;
-    flex-direction: row;
-    max-width: 80%;
-
-    p {
-      font-size: 14px;
-      color: #7e888b;
-    }
-  }
-}
 </style>
---
+++ src/components/other/Comment.vue
@@ -0,0 +1,180 @@
+<template>
+  <div id="Comment">
+    <!--    评论输入框-->
+    <el-card class="comment-card" justify="space-between">
+      <p>发表评论</p>
+      <el-input
+        v-model="commentContext.comment"
+        type="textarea"
+        maxlength="1000"
+        show-word-limit
+        :rows="commentInputRow"
+        placeholder="请发表有价值的评论， 博客评论不欢迎灌水，良好的社区氛围需大家一起维护。"
+        style="border-style: none;"
+      >
+      </el-input>
+      <el-row style="margin-top: 10px;" justify="space-between">
+        <!--        todo 添加表情包和图片功能-->
+        <el-col span="12">
+        </el-col>
+        <el-col span="12">
+          <el-button type="success" icon="Check" @click="commentBlog">发表</el-button>
+        </el-col>
+      </el-row>
+
+      <el-divider content-position="left">评论列表</el-divider>
+      <el-row justify="space-between" v-for="item in commentList" :key="item" style="margin-bottom: 10px">
+        <el-col span="16" class="comment-text-left-col">
+          <el-avatar :size="30" :src="item.headImgUrl === null ? circleUrl :item.headImgUrl" fit="cover"/>
+          <div style="flex-direction:column;margin-left: 10px">
+            <p>{{ item.createUserName === null ? item.ipAddress : item.createUserName }} | {{ item.browserModel }} |
+              {{ item.createDate }}</p>
+            <span class="comment-text">{{ item.comment }}</span>
+          </div>
+        </el-col>
+        <el-col span="8" style="flex-direction:row;margin-top: 10px">
+          <el-button icon="Edit" size="small">回复</el-button>
+          <el-button icon="Check" size="small"/>
+        </el-col>
+      </el-row>
+      <el-pagination
+        v-model:currentPage="commentRequest.pageIndex"
+        :page-size="commentRequest.pageSize"
+        small="small"
+        background
+        layout="total, prev, pager, next"
+        :total="commentRequest.total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        v-show="commentRequest.total >10"
+      />
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { commentBlogApi, commentListApi } from '@/utils/api';
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Comment',
+  props: ['blog'],
+  setup(props) {
+    console.log(props)
+  },
+  data() {
+    return {
+      commentContext: {
+        blogId: '',
+        comment: '',
+        browserModel: ''
+      },
+      commentInputRow: 3,
+      commentRequest: {
+        pageIndex: 1,
+        pageSize: 10,
+        total: 0
+      },
+      commentList: [],
+      userinfo: {},
+      blogInfo: {
+        id: '',
+        content: '',
+        createUserId: ''
+      }
+    }
+  },
+  methods: {
+    async getCommentList() {
+      this.commentRequest.blogId = this.blogInfo.id
+      const { data: result } = await commentListApi(this.commentRequest)
+      this.commentList = result.data
+      this.commentRequest.pageSize = result.pageSize
+      this.commentRequest.pageIndex = result.pageIndex
+      this.commentRequest.total = result.total
+    },
+    async commentBlog() {
+      this.commentContext.blogId = this.blogInfo.id
+      this.commentContext.blogAuthorId = this.blogInfo.createUserId
+      this.commentContext.browserModel = this.getExplorer()
+      await commentBlogApi(this.commentContext)
+      await this.getCommentList()
+      this.commentContext.comment = ''
+    },
+    handleSizeChange(val) {
+      this.commentRequest.pageSize = val
+      this.getCommentList()
+    },
+    handleCurrentChange(val) {
+      this.commentRequest.pageIndex = val
+      this.getCommentList()
+    },
+    getExplorer() {
+      const explorer = window.navigator.userAgent;
+      //Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36
+      let start = 0;
+      const compare = function (s) {
+        start = explorer.indexOf(s)
+        return (start >= 0);
+      };
+      const ie11 = (function () {
+        return ('ActiveXObject' in window)
+      })();
+      if (compare('MSIE') || ie11) {
+
+      } else if (compare('Firefox') && !ie11) {
+
+      } else if (compare('Chrome') && !ie11) {
+
+      } else if (compare('Opera') && !ie11) {
+
+      } else if (compare('Safari') && !ie11) {
+
+      }
+      return explorer.substring(start);
+    }
+  },
+  mounted() {
+    this.userinfo = JSON.parse(window.sessionStorage.getItem('userinfo'))
+    // this.blog.id = this.$route.query.blogId
+    // this.blog = props
+    this.getCommentList()
+    console.log('初始化')
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.comment-card {
+  display: inline-block;
+  text-align: left;
+  width: 100%;
+  margin-top: 10px;
+
+  .comment-text {
+    line-height: 25px;
+    //font-size: 14px;
+    //color: #a2b0b7;
+    word-break: break-word;
+    // 文字自动换行
+    white-space: normal;
+  }
+
+  .el-avatar {
+    margin-top: 10px;
+    min-width: 30px;
+    min-height: 30px
+  }
+
+  .comment-text-left-col {
+    display: flex;
+    flex-direction: row;
+    max-width: 80%;
+
+    p {
+      font-size: 14px;
+      color: #7e888b;
+    }
+  }
+}
+</style>
