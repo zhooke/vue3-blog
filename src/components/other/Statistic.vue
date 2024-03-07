8036f0e9affc6c092b2687272cceb0acd2f6e572
zhooke <709688530@qq.com>
2024-03-07 09:47:23 +0800

    【fix】修改图表显示

--- src/components/other/Statistic.vue
+++ src/components/other/Statistic.vue
@@ -1,124 +1,127 @@
 <template>
-<h1>hello world</h1>
-  <div id="main"></div>
+  <!-- <h1>hello world</h1> -->
+  <div id="box" ref="box" style="width: 100%;height: 100%;"></div>
 </template>
 
-<script>
-import echarts from "echarts";
+<script setup>
+import { onMounted, ref } from 'vue';
 
-const chartDom = document.getElementById('main');
-const myChart = echarts.init(chartDom);
-let option;
+//引入echarts
 
-const colors = ['#5470C6', '#91CC75', '#EE6666'];
-option = {
-  color: colors,
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross'
-    }
-  },
-  grid: {
-    right: '20%'
-  },
-  toolbox: {
-    feature: {
-      dataView: { show: true, readOnly: false },
-      restore: { show: true },
-      saveAsImage: { show: true }
-    }
-  },
-  legend: {
-    data: ['Evaporation', 'Precipitation', 'Temperature']
-  },
-  xAxis: [
-    {
-      type: 'category',
-      axisTick: {
-        alignWithLabel: true
-      },
-      // prettier-ignore
-      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value',
-      name: 'Evaporation',
-      position: 'right',
-      alignTicks: true,
-      axisLine: {
-        show: true,
-        lineStyle: {
-          color: colors[0]
-        }
-      },
-      axisLabel: {
-        formatter: '{value} ml'
+import * as echarts from 'echarts';
+
+let box = ref(null)
+
+onMounted(async () => {
+  let myChart = echarts.init(box.value);
+  let option;
+
+  const colors = ['#5470C6', '#91CC75', '#EE6666'];
+  option = {
+    color: colors,
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross'
       }
     },
-    {
-      type: 'value',
-      name: 'Precipitation',
-      position: 'right',
-      alignTicks: true,
-      offset: 80,
-      axisLine: {
-        show: true,
-        lineStyle: {
-          color: colors[1]
-        }
-      },
-      axisLabel: {
-        formatter: '{value} ml'
+    grid: {
+      right: '20%'
+    },
+    toolbox: {
+      feature: {
+        dataView: { show: true, readOnly: false },
+        restore: { show: true },
+        saveAsImage: { show: true }
       }
     },
-    {
-      type: 'value',
-      name: '温度',
-      position: 'left',
-      alignTicks: true,
-      axisLine: {
-        show: true,
-        lineStyle: {
-          color: colors[2]
+    legend: {
+      data: ['Evaporation', 'Precipitation', 'Temperature']
+    },
+    xAxis: [
+      {
+        type: 'category',
+        axisTick: {
+          alignWithLabel: true
+        },
+        // prettier-ignore
+        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        name: 'Evaporation',
+        position: 'right',
+        alignTicks: true,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: colors[0]
+          }
+        },
+        axisLabel: {
+          formatter: '{value} ml'
         }
       },
-      axisLabel: {
-        formatter: '{value} °C'
+      {
+        type: 'value',
+        name: 'Precipitation',
+        position: 'right',
+        alignTicks: true,
+        offset: 80,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: colors[1]
+          }
+        },
+        axisLabel: {
+          formatter: '{value} ml'
+        }
+      },
+      {
+        type: 'value',
+        name: '温度',
+        position: 'left',
+        alignTicks: true,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: colors[2]
+          }
+        },
+        axisLabel: {
+          formatter: '{value} °C'
+        }
       }
-    }
-  ],
-  series: [
-    {
-      name: 'Evaporation',
-      type: 'bar',
-      data: [
-        2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
-      ]
-    },
-    {
-      name: 'Precipitation',
-      type: 'bar',
-      yAxisIndex: 1,
-      data: [
-        2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
-      ]
-    },
-    {
-      name: 'Temperature',
-      type: 'line',
-      yAxisIndex: 2,
-      data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-    }
-  ]
-};
-
-option && myChart.setOption(option);
-
+    ],
+    series: [
+      {
+        name: 'Evaporation',
+        type: 'bar',
+        data: [
+          2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
+        ]
+      },
+      {
+        name: 'Precipitation',
+        type: 'bar',
+        yAxisIndex: 1,
+        data: [
+          2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
+        ]
+      },
+      {
+        name: 'Temperature',
+        type: 'line',
+        yAxisIndex: 2,
+        data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+      }
+    ]
+  };
+  myChart.setOption(option);
+})
 </script>
 
-<style scoped lang="less">
-
-</style>
+<style scoped lang="less"></style>
