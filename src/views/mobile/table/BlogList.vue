2cef97ca7e70d6c9bbb5f692504d464f272b7b34
zhooke <709688530@qq.com>
2024-06-14 16:03:15 +0800

    【feature】添加日历选项

--- src/views/mobile/table/BlogList.vue
+++ src/views/mobile/table/BlogList.vue
@@ -1,22 +1,33 @@
 <template>
-  <van-nav-bar title="最新文章" />
+  <van-nav-bar left-arro title="最新文章">
+    <template #right>
+      <van-icon name="calendar-o" size="18" @click="showCalendarFun"/>
+    </template>
+  </van-nav-bar>
+
   <van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="getBlogList">
     <van-cell v-for="blog in blogList" :key="blog">
       <van-text-ellipsis :content="blog.title" @click="blogCardClick(blog.id)"></van-text-ellipsis>
     </van-cell>
-    <van-back-top />
+    <van-back-top/>
   </van-list>
+
+  <van-calendar v-model:show="showCalendar" :max-date="maxDate" :min-date="minDate" switch-mode="year-month"
+                @confirm="calendarConfirmFun"/>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { getBlogListApi } from '@/utils/api.js';
-import { showNotify } from 'vant';
+import {ref} from 'vue';
+import {getBlogListApi} from '@/utils/api.js';
+import {showNotify} from 'vant';
 import router from '@/router/index.js';
 
 let list = ref([]);
 let loading = ref(false);
 let finished = ref(false);
+let showCalendar = ref(false);
+let minDate = new Date(2020, 0, 1);
+let maxDate = new Date();
 let blogList = ref([]);
 let blogRequest = {
   pageIndex: 1,
@@ -27,7 +38,7 @@ let blog = ref({})
 function getBlogList() {
   getBlogListApi(blogRequest).then(response => {
     if (response.data.code !== 200) {
-      return showNotify({ type: 'danger', message: response.data.data });
+      return showNotify({type: 'danger', message: response.data.data});
     }
     blogList.value.push(...response.data.data);
     loading.value = false;
@@ -40,9 +51,17 @@ function getBlogList() {
 }
 
 function blogCardClick(value) {
-  router.push({ path: '/mobile/blog/read', query: { blogId: value } })
+  router.push({path: '/mobile/blog/read', query: {blogId: value}})
 }
 
+function showCalendarFun() {
+  showCalendar.value = !showCalendar.value;
+}
+
+function calendarConfirmFun(value) {
+  showCalendarFun()
+  console.log(value)
+}
 </script>
 
 <style lang="less" scoped>
